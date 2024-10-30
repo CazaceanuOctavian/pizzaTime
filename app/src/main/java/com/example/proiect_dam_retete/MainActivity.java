d6e98d57ed0ce70696109b0fb7e97719bc52be0e
Complainer <o.cazatavian@gmail.com>
2024-10-30 15:17:16 +0200

    added navbar skeleton

--- app/src/main/java/com/example/proiect_dam_retete/MainActivity.java
+++ app/src/main/java/com/example/proiect_dam_retete/MainActivity.java
@@ -6,27 +6,39 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavigationView mainNav;
+    Toolbar mainToolbar;
+    DrawerLayout mainDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
 
         //TODO --> move this to the navview
         Button navigateButton = findViewById(R.id.main_activity_launch_recipe_list_btn);
-
+        this.mainNav = findViewById(R.id.cazaceanu_octavian_main_navview);
+        this.mainToolbar = findViewById(R.id.cazaceanu_octavian_main_toolbar);
+        this.mainDrawerLayout = findViewById(R.id.cazaceanu_octavian_main_drawer_layout);
+        setSupportActionBar(mainToolbar);
+        //setup the toolbar-navview combo
+        ActionBarDrawerToggle toggleObject = new ActionBarDrawerToggle(
+            this, mainDrawerLayout, mainToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        );
+        toggleObject.syncState();
         // Set click listener for the button
         navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
