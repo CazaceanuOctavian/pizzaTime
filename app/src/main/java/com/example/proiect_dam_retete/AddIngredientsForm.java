021b6ba24181b8f5384a259d9b51566b83d8bd27
unknown <dinita.cosmina.nicola@gmail.com>
2024-10-31 13:42:45 +0200

    mergeCosminaIntoMain

--- app/src/main/java/com/example/proiect_dam_retete/AddIngredientsForm.java
+++ app/src/main/java/com/example/proiect_dam_retete/AddIngredientsForm.java
@@ -1,6 +1,8 @@
 package com.example.proiect_dam_retete;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AddIngredientsForm extends AppCompatActivity {
-
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,16 @@ public class AddIngredientsForm extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        spinner=findViewById(R.id.dinita_cosmina_spinner);
+        EIngredients[] ingredients = EIngredients.values();
+        String[] ingredientNames = new String[ingredients.length];
+
+        for (int i = 0; i < ingredients.length; i++) {
+            ingredientNames[i] = ingredients[i].name();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, ingredientNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
