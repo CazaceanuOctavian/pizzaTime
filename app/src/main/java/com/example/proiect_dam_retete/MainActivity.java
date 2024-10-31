73ee2a64366a153afd11d02946d02fc8f164bfe9
Complainer <o.cazatavian@gmail.com>
2024-10-31 13:06:22 +0200

    post-merge

--- app/src/main/java/com/example/proiect_dam_retete/MainActivity.java
+++ app/src/main/java/com/example/proiect_dam_retete/MainActivity.java
@@ -9,9 +9,7 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
